22cb8637bb0e2788b7a8fda6c828e8d5495e7792
Simba Zhang <solderzzc@gmail.com>
2018-06-16 04:55:11 +0800

    The core is working.

--- fast_facenet/gpu_deploy/deploy.py
+++ fast_facenet/gpu_deploy/deploy.py
@@ -1,5 +1,8 @@
-
+import cv2
 import tvm
+import numpy as np
+
+from tvm.contrib import rpc, util, graph_runtime
 
 # tvm module for compiled functions.
 loaded_lib = tvm.module.load('/root/net1.tar')
@@ -8,13 +11,30 @@ loaded_json = open("/root/net1").read()
 # parameters in binary
 loaded_params = bytearray(open("/root/net1.params", "rb").read())
 
-fcreate = tvm.get_global_func("tvm.graph_runtime.create")
+ctx = tvm.cl(0)
+
+mod = graph_runtime.create(loaded_json, loaded_lib, ctx)
+mod.load_params(loaded_params)
+
+a = np.random.uniform(size=(1,3,112,112)).astype('float32')
+
+mod.run(data=a)
+
+out = mod.get_output(0, tvm.nd.empty((128,)))
+print(out)
+
+#for i in range(0,100):
+  
+
+#set_input, get_output, run = gmodule["set_input"], gmodule["get_output"], gmodule["run"]
+#gmodule["load_params"](loaded_params)
+
+#set_input("x", tvm.nd.array(x_np))
+#run()
+exit(0)
+
 ctx = tvm.gpu(0)
 gmodule = fcreate(loaded_json, loaded_lib, ctx.device_type, ctx.device_id)
-set_input, get_output, run = gmodule["set_input"], gmodule["get_output"], gmodule["run"]
-
-gmodule["load_params"](loaded_params)
-
 #set_input("x", tvm.nd.array(x_np))
 #run()
 #out = tvm.nd.empty(shape)
