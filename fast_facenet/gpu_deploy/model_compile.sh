53bad1764fba2f66fcf985b2ae03122a6bee2acc
Simba Zhang <solderzzc@gmail.com>
2018-06-15 12:12:59 -0700

    Compile graph on x86 for tvm is working.

---
+++ fast_facenet/gpu_deploy/model_compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -v `pwd`/export:/export -v `pwd`/../model-y1-test2:/root/model-y1-test2 -v `pwd`/../gpu_deploy/convert_net_1.py:/root/convert_net_1.py -v `pwd`/runtime.sh:/root/runtime.sh -ti solderzzc/rocketchat:tvm_06152018_x86 /root/runtime.sh
---
+++ fast_facenet/gpu_deploy/runtime.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /root
+python convert_net_1.py
+
+mv ./net1.tar /export/
+mv ./net1.params /export/
