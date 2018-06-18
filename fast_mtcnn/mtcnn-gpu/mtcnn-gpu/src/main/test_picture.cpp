72716ce2c0ed075bef82e6b8cdd5dc1776a1d4b0
Simba Zhang <solderzzc@gmail.com>
2018-06-19 03:59:57 +0800

    Start to integrate c++ into python through pybind11.

---
+++ fast_mtcnn/mtcnn-gpu/mtcnn-gpu/src/main/test_picture.cpp
@@ -0,0 +1,121 @@
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "mtcnn.hpp"
+#include "utils.hpp"
+
+int main(int argc, char * argv[])
+{
+    std::string type = "mxnet";
+    std::string fpath = "test.jpg";
+    std::string model_dir = "../models"; 
+    std::string out_dir = "../outputs";    
+       
+    int save_chop = 0;
+
+    int res;
+
+    while ((res = getopt(argc,argv,"f:m:o:t:s")) != -1) {
+        switch (res) {
+            case 'f':
+                fpath = std::string(optarg);
+                break;
+            case 'm':
+                model_dir = std::string(optarg);
+                break;
+            case 'o':
+                out_dir = std::string(optarg);
+                break;
+            case 't':
+                type = std::string(optarg);
+                break;
+            case 's':
+                save_chop = 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    //cv::namedWindow(fpath, cv::WINDOW_AUTOSIZE);
+
+
+
+    Mtcnn * p_mtcnn = MtcnnFactory::CreateDetector(type);
+
+    if (p_mtcnn == nullptr) {
+        std::cerr << type << " is not supported" << std::endl;
+        std::cerr << "supported types: ";
+        std::vector<std::string> type_list = MtcnnFactory::ListDetectorType();
+
+        for (unsigned int i = 0; i < type_list.size(); i++)
+            std::cerr << " " << type_list[i];
+
+        std::cerr << std::endl;
+        exit(1);
+    }
+
+    p_mtcnn->LoadModule(model_dir);
+
+    // read image
+    cv::Mat frame = cv::imread(fpath);
+    if (!frame.data) {
+        std::cerr << "failed to read image file: " << fpath << std::endl;
+        exit(1);
+    }
+
+    int cycle = 0;
+    while( cycle++ < 100000) {
+
+        std::vector<face_box> face_info;    
+        unsigned long start_time = get_cur_time();
+        p_mtcnn->Detect(frame,face_info);
+        unsigned long end_time = get_cur_time();
+
+        for(unsigned int i = 0; i < face_info.size(); i++) {
+            face_box& box = face_info[i];
+            std::ostringstream oss;
+            oss << "face id: " << i << ". box: " << "(" << box.x0 << ", " << box.y0 << ")";
+            std::cout << oss.str() << std::endl;
+            printf("face %d: x0,y0 %2.5f %2.5f  x1,y1 %2.5f  %2.5f conf: %2.5f\n",i,
+                    box.x0,box.y0,box.x1,box.y1, box.score);
+            printf("landmark: ");
+
+            for(unsigned int j = 0; j < 5; j++)
+                printf(" (%2.5f %2.5f)",box.landmark.x[j], box.landmark.y[j]);
+
+            printf("\n");
+
+            if (save_chop) {
+                cv::Mat corp_img = frame(cv::Range(box.y0, box.y1), cv::Range(box.x0, box.x1));
+                auto outputs = str_split(fpath, '/');
+                std::string fname = outputs.back();
+                std::ostringstream oname;
+                oname << out_dir << "/" << "chop_" << i << "_" << fname;
+                if (!cv::imwrite(oname.str(), corp_img)) {
+                    std::cerr << "can't save chopped image: " << oname.str() << std::endl;
+                }
+            }
+
+            // draw box
+            cv::rectangle(frame, cv::Point(box.x0, box.y0), cv::Point(box.x1, box.y1), cv::Scalar(0, 255, 0), 2);
+
+            // draw landmark  scalar: BGR
+            for (int l = 0; l < 5; l++) {
+                cv::circle(frame, cv::Point(box.landmark.x[l],box.landmark.y[l]), 2, cv::Scalar(0, 0, 255), 2);
+            }
+        }
+
+        std::cout << "total detected: " << face_info.size() << " faces. used "
+             << (end_time-start_time) << " us" << std::endl;
+
+    }
+ 
+    //cv::imshow(fpath, frame);
+    //cv::waitKey(0);
+
+    return 0;
+}
---
+++ fast_mtcnn/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [[ -z "$1" ]]; then
+  docker run --privileged -v `pwd`/:/root/test -v /sys/class/misc/mali0:/sys/class/misc/mali0 -v/dev/mali0:/dev/mali0 -v`pwd`/export:/export -ti solderzzc/rocketchat:mxnet_mali /bin/bash
+else
+  docker run --privileged -v `pwd`/:/root/test -v /sys/class/misc/mali0:/sys/class/misc/mali0 -v/dev/mali0:/dev/mali0 -v`pwd`/export:/export -ti $1 /bin/bash
+fi
