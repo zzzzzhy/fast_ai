5de4aff08aee0d7181f6fc0c6c27b89872d788f8
Simba Zhang <solderzzc@gmail.com>
2018-06-19 08:11:05 +0800

    To use json format as resut.

--- fast_mtcnn/mtcnn-gpu/mtcnn-gpu/tests/test.py
+++ fast_mtcnn/mtcnn-gpu/mtcnn-gpu/tests/test.py
@@ -1,5 +1,10 @@
 import face_detection as m
-m.load()
-result =m.detect()
-print(result)
-m.test()
+import json
+m.load('../models')
+result =m.detect('test.jpg','./output')
+#print(result)
+
+obj = json.loads(result)
+
+print(obj['result'][0]['score'])
+#m.test()
