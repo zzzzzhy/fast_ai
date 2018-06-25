f9596817a04dcbec1baf978ed910c4565569c855
Simba Zhang <solderzzc@gmail.com>
2018-06-26 03:54:10 +0800

    Added modified deploy.sh

--- fast_od/deploy.sh
+++ fast_od/deploy.sh
@@ -3,9 +3,12 @@
 docker run --rm --privileged \
      -v /sys/class/misc/mali0:/sys/class/misc/mali0 \
      -v/dev/mali0:/dev/mali0 \
+     -v `pwd`/:/root/test \
      -v `pwd`/export:/export \
      -v `pwd`/runtime_gpu.sh:/root/runtime_gpu.sh \
      -v `pwd`/deploy_od.py:/root/deploy_od.py \
+     -v `pwd`/export/_convert.so:/root/_convert.so \
+     -v `pwd`/export/convert.py:/root/convert.py \
      -v `pwd`/export/od.tar:/root/od.tar \
      -v `pwd`/export/od.cfg:/root/od.cfg \
      -v `pwd`/export/libruntime.so:/root/libruntime.so \
