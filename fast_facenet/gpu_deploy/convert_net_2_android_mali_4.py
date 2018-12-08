45c8d74ab63d72023fc3c9b947865629a0fb89db
Simba Zhang <solderzzc@gmail.com>
2018-12-07 19:15:22 -0800

    To commit batch4 version of test code.

---
+++ fast_facenet/gpu_deploy/convert_net_2_android_mali_4.py
@@ -0,0 +1,32 @@
+# some standard imports
+import mxnet as mx
+import nnvm
+import tvm
+import numpy as np
+
+def get_feature(self, aligned):
+  #face_img is bgr image
+  #print(nimg.shape)
+  input_blob = np.expand_dims(aligned, axis=0)
+  data = mx.nd.array(input_blob)
+  db = mx.io.DataBatch(data=(data,))
+
+def convert(net, params, data_shape, dtype, target_host):
+    # compile
+    opt_level = 2 if dtype == 'float32' else 1
+    with nnvm.compiler.build_config(opt_level=opt_level):
+        graph, lib, params = nnvm.compiler.build(
+            net, tvm.target.mali(), shape={"data": data_shape}, params=params,
+            dtype=dtype, target_host=target_host)
+
+    lib.export_library('./net2.tar')
+
+    with open("./net2", "w") as fo:
+        fo.write(graph.json())
+    with open("./net2.params", "wb") as fo:
+        fo.write(nnvm.compiler.save_param_dict(params))
+
+net, params, auxs = mx.model.load_checkpoint('./model-r50-am-lfw/model', 0)
+nnvm_net, nnvm_params = nnvm.frontend.from_mxnet(net, params, auxs)
+
+convert(nnvm_net, nnvm_params, (4, 3, 112, 112) ,'float32','llvm -target=arm64-linux-android -mattr=+neon')
---
+++ fast_facenet/gpu_deploy/model_compile2_android_mali_4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run --rm -v `pwd`/export:/export -v `pwd`/../model-r50-am-lfw:/root/model-r50-am-lfw -v `pwd`/../gpu_deploy/convert_net_2_android_mali_4.py:/root/convert_net_2.py -v `pwd`/runtime2.sh:/root/runtime2.sh -ti solderzzc/rocketchat:tvm_12072018_x86 /root/runtime2.sh
+
+#docker build -t solderzzc/rocketchat:tvm_06142018_net2 .
+#docker push solderzzc/rocketchat:tvm_06142018_net2
